a1695fb463c52d739124a72a8ee8d13d7a32e42c
Roman Kudlay <roman@kudlay.pro>
2023-09-22 22:52:25 +0300

    Updated flash script

--- files/thr_flash.sh
+++ files/thr_flash.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ ! -f /dev/sda1 ]; then
+  echo Error: THR Board not found
+  exit 1
+fi;
 wget https://github.com/krom/kingroon_kp3s_pro_v2_wiki/raw/main/files/klipper.uf2
 mkdir /mnt/head/
 mount -t vfat /dev/sda1 /mnt/head/
